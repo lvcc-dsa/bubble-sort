c2a7e59cf0e943e215f2a8a99d07d4ee2066b2ea
R. O. Brave <arrowbrave@gmail.com>
2016-09-23 21:14:02 +0800

    created BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,43 @@
+/**
+* BUBBLE SORT is based on the idea of repeatedly comparing pairs of 
+* adjacent elements, then switching positions if they exist in the wrong order.
+*
+* @author  [your full name here]
+* @link    [full github url] ex. http://username.github.com/bubble-sort/
+* @version [version number]
+* @since   [last updated dd/mm/yyyy]
+*/
+
+import java.util.Scanner;
+import java.util.Random;
+public class BubbleSort {
+	public static void main(String[] args) {
+		int size, x, y, swap, array[];
+		Scanner input = new Scanner(System.in);
+		Random rand = new Random();
+		System.out.print("Enter array size to sort: ");
+		size = input.nextInt();
+		array = new int[size];
+		System.out.println();
+		System.out.println(size + " random numbers generated.");
+		for (x = 0; x < size; x++) {
+			array[x] = rand.nextInt(1000);
+			System.out.print(array[x] + " ");
+		}
+		System.out.println();
+		for (x = 0; x < size-1; x++) {
+			for (y = 0; y < size-x-1; y++) {
+				if (array[y] > array[y+1]) {
+					swap = array[y];
+					array[y] = array[y+1];
+					array[y+1] = swap;
+				}
+			}
+		}
+		System.out.println();
+		System.out.println("Bubble Sorting... Done!");
+		for (x = 0; x < size; x++) {
+			System.out.print(array[x] + " ");
+		}
+	}
+}
